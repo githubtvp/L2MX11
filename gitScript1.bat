9bacef31eb40acc35720ce5579c5e53b71701d12
githubtvp <tvp1640@gmail.com>
2024-01-10 22:40:51 +0530

    Saving latest Updates with script

---
+++ gitScript1.bat
@@ -0,0 +1,14 @@
+
+
+
+rem Simple Git Script
+
+cd C:\Users\Varadha\Desktop\MX11
+
+git pull origin main
+git add .
+git commit -a -m "Saving latest Updates with script"
+git push origin main
+
+echo "Git commands executed successfully."
+cmd
